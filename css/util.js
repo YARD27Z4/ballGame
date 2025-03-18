e3f8907e421ae7792ac4ab73278e71b5e8ece6f0
Yarden Koriat <yardenna247@gmail.com>
2025-03-18 18:05:53 +0200

    Add random color change on click

--- css/util.js
+++ css/util.js
@@ -3,3 +3,7 @@
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 'use strict'
 
 function onBallClick() {
-  var ball = document.querySelector(".ball");
-  var size = parseInt(ball.style.width) || 100;
-  size += 50;
-  if (size > 400) size = 100;
+  var ball = document.querySelector(".ball")
+  var size = parseInt(ball.style.width) || 100
+  size += getRandomInt(20, 60)
+  if (size > 400) size = 100
   ball.style.width = size + "px";
   ball.style.height = size + "px";
   ball.innerText = size;
+  ball.style.backgroundColor = getRandomColor();
 }
+
