757ef8954083f99824e664b8bb447b63269f5b07
Yarden Koriat <yardenna247@gmail.com>
2025-03-18 16:55:00 +0200

    Ball size

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 'use strict'
 
 function onBallClick() {
+  var ball = document.querySelector(".ball");
+  var size = parseInt(ball.style.width) || 100;
+  size += 50;
+  if (size > 400) size = 100;
+  ball.style.width = size + "px";
+  ball.style.height = size + "px";
+  ball.innerText = size;
 }
-
